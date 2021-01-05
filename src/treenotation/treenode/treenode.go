5100bcbbb5f6f36ff037fa41d717f5fb1e9c112d
Denis Papathanasiou <denis@papathanasiou.org>
2021-01-05 12:08:19 -0500

    Following practical advice in the FAQ, added an ignoreable symbol for windows inputs

--- src/treenotation/treenode/treenode.go
+++ src/treenotation/treenode/treenode.go
@@ -8,8 +8,11 @@ type TreeNode struct {
 }
 
 const (
-	// NodeBreakSymbol delimits nodes (TODO: find an os-independent definition)
+	// NodeBreakSymbol delimits nodes (lines)
 	NodeBreakSymbol = "\n"
+
+	// IgnoreableSymbol is for Windows, so that NodeBreakSymbol does not have to change based on os type
+	IgnoreableSymbol = "\r"
 
 	// EdgeSymbol is used to indicate the parent/child relationship between nodes
 	EdgeSymbol = " "
