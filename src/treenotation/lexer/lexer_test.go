8f44b86b874053b151145d702baad5294e23dadf
Denis Papathanasiou <denis@papathanasiou.org>
2021-01-05 18:04:08 -0500

    Plus a basic test of the lexer (TODO: add more cases, particularly strings with \r, etc)

---
+++ src/treenotation/lexer/lexer_test.go
@@ -0,0 +1,27 @@
+package lexer
+
+import (
+	"testing"
+)
+
+func TestInputLexer(t *testing.T) {
+	testInput := `if true
+  print Hello world`
+
+	testOutputs := []string{
+		"if true",
+		"  print Hello world",
+	}
+
+	l := NewLexer(testInput)
+
+	for i, testOutput := range testOutputs {
+		node, _ := l.NextNode()
+
+		if node != testOutput {
+			t.Fatalf("tests[%d] failed: expected=%s, actual=%s",
+				i, testOutput, node)
+		}
+
+	}
+}
